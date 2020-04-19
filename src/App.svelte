ab5c0e47eaa5ae5eb96a1ce2886151b42a5a4f5d
Adam Pond <adam.pond@hotmail.co.uk>
2020-04-19 18:22:17 +0100

    added rest countdown

--- src/App.svelte
+++ src/App.svelte
@@ -2,34 +2,53 @@
 <main>
 	<h1>Stretch Timer</h1>
 
-	<input type="number" placeholder="Interval">
-
-	<input type="number" placeholder="Breaks">
-
-
-
-	<button disabled={timerRunning || pause} on:click={startStopwatch}>Start</button>
-	<button on:click={pauseStopwatch}>{pause ? "Continue" : "Pause"}</button>
-	<button on:click={resetStopwatch}>Reset Timer</button>
-
-	<div>{stopwatchValue}</div>
+	<div class="input-container">
+		<label for="stretch-hold">Hold stretch for?</label>
+		<input id="stretch-hold" bind:value={stretchHoldTime} type="number">
+	</div>
+	<div class="input-container">
+		<label for="rest-time">How much rest?</label>
+		<input id="rest-time"bind:value={restTimeInput} type="number">
+	</div>
 
 
+
+	<div class="button-container">
+		<button disabled={timerRunning || pause} on:click={startStopwatch}>Start</button>
+		<button on:click={pauseStopwatch}>{pause ? "Continue" : "Pause"}</button>
+		<button on:click={resetStopwatch}>Reset Timer</button>
+	</div>
+
+	<div>Stretch Timer = {mins} : {secs < 10 ? 0 : ''}{secs}</div>
+	<div>Rest Timer = {restTime ? restTime : 0}</div>
+
+	<div class="message">{message}</div>
 </main>
 
 <script>
 
 let stopwatchValue = 0
-let time
+let interval
+let intervalRest
 let pause = false
 let timerRunning = false
+let stretchHoldTime
+let message = ''
+let secs = 0
+let restTime = '0'
+let restTimeInput
+
+$: restTime = restTimeInput
+$: mins = Math.floor((stopwatchValue % 3600) / 60)
+$: secs = Math.floor(stopwatchValue % 60)
 
 const startStopwatch = () => {
+	message = 'Stretch'
 	timerRunning = true
 	pause = false
-	time = setInterval(function(){ stopwatchValue += 1 }, 1000);
+	interval = setInterval(function(){ stopwatchValue += 1 }, 1000);
 }
-const pauseStopwatch = () => {
+const pauseStopwatch = (restTime) => {
 	if (stopwatchValue === 0) {
 		return
 	}
@@ -37,17 +56,40 @@ const pauseStopwatch = () => {
 		startStopwatch()
 	} else {
 		timerRunning = false
-		clearInterval(time)
+		message = 'Timer Paused'
+		clearInterval(interval)
 		pause = true
 	}
 }
 
 const resetStopwatch = () => {
+	message = ''
 	timerRunning = false
 	pause = false
-	clearInterval(time)
+	clearInterval(interval)
 	stopwatchValue = 0
 }
+
+
+$: if (secs === stretchHoldTime) {
+	restTimerStartCountdown()
+}
+
+$: if (restTime === 0) {
+		clearInterval(intervalRest)
+		startStopwatch()
+		restTime = restTimeInput
+	}
+
+const restTimerStartCountdown = () => {
+	message = 'rest'
+	pauseStopwatch()
+	intervalRest = setInterval(function(){ restTime -= 1 }, 1000);
+	
+}
+
+
+
 </script>
 
 
@@ -63,4 +105,27 @@ const resetStopwatch = () => {
 
 <style>
 
+	main {
+		width: 600px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	input {
+		width: 100px;
+		margin: 10px 20px;
+	}
+	.input-container {
+		display: flex;
+		align-items: baseline;
+    justify-content: center;
+	}
+
+	.message {
+		font-size: 30px;
+		color: red;
+	}
 </style>
